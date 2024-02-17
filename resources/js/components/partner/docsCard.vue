1677014a2e4e7931e7f9fbb9186f8047ecf487a8
codershisho <codershisho@gmail.com>
2024-02-17 10:12:03 +0900

    実装

--- resources/js/components/partner/docsCard.vue
+++ resources/js/components/partner/docsCard.vue
@@ -4,9 +4,30 @@
       <o-savebtn @click="fileUpload">保存する</o-savebtn>
     </div>
     <div>
-      <input type="file" accept="application/pdf" @change="fileSelected" />
-      <div v-for="(document, i) in documents" :key="i">
-        <div class="py-2 tw-cursor-pointer" @click="read(document)">{{ document.fileName }}</div>
+      <v-btn
+        variant="flat"
+        rounded="1"
+        color="primary"
+        prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
+        @click="selectFile"
+      >
+        ファイル選択
+      </v-btn>
+      <div class="tw-text-sm py-2">{{ file1Name }}</div>
+      <input
+        style="display: none"
+        ref="file1"
+        type="file"
+        accept="application/pdf"
+        @change="fileSelected"
+      />
+      <v-divider class="my-2"></v-divider>
+      <div class="tw-text-sm py-2">履歴</div>
+      <div v-for="(document, i) in documents" :key="i" class="d-flex align-center tw-border-b-2">
+        <div class="tw-w-1/12 tw-text-right pr-3">{{ i }}：</div>
+        <div class="tw-w-11/12 py-2 tw-cursor-pointer" @click="read(document)">
+          {{ document.fileName }}
+        </div>
       </div>
     </div>
   </o-card>
@@ -20,6 +41,8 @@ import { IDocument } from '@/types/partner';
 const props = defineProps({
   id: String,
 });
+const file1 = ref();
+const file1Name = ref('');
 const fileInfo = ref('');
 const documents = ref<IDocument[]>();
 const document = ref<IDocument>({
@@ -40,6 +63,7 @@ const search = async () => {
 
 const fileSelected = async (event: any) => {
   fileInfo.value = event.target.files[0];
+  file1Name.value = event.target.files[0].name;
 };
 
 const fileUpload = async () => {
@@ -48,10 +72,14 @@ const fileUpload = async () => {
   document.value = await service.uploadBasic(Number(props.id), formData);
   await search();
   fileInfo.value = '';
+  file1Name.value = '';
 };
 
 const read = (document: IDocument) => {
   window.open(import.meta.env.VITE_APP_URL + document.basic_path, '_blank');
+};
+const selectFile = () => {
+  file1.value.click();
 };
 </script>
 
--- resources/js/components/project/agreement.vue
+++ resources/js/components/project/agreement.vue
@@ -5,31 +5,68 @@
     </div>
     <div>
       <div class="tw-text-sm py-2">個別契約書</div>
-      <v-btn
-        color="primary"
-        variant="flat"
-        rounded="1"
-        prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
-        :disabled="!isUploaded"
-        @click="read"
-      >
-        参照
-      </v-btn>
-      <input type="file" accept="application/pdf" @change="fileSelected" />
+      <div>
+        <v-btn
+          class="mr-5"
+          variant="flat"
+          rounded="1"
+          color="primary"
+          prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
+          @click="selectAgreement"
+        >
+          ファイル選択
+        </v-btn>
+        <v-btn
+          color="primary"
+          variant="flat"
+          rounded="1"
+          prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
+          :disabled="!isUploaded"
+          @click="read"
+        >
+          参照
+        </v-btn>
+        <input
+          style="display: none"
+          ref="file1"
+          type="file"
+          accept="application/pdf"
+          @change="fileSelected"
+        />
+      </div>
+      <div class="tw-text-sm py-2">{{ file1Name }}</div>
     </div>
     <div>
       <div class="tw-text-sm py-2">見積書</div>
-      <v-btn
-        color="primary"
-        variant="flat"
-        rounded="1"
-        prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
-        :disabled="!isUploadedEstimate"
-        @click="readEstimate"
-      >
-        参照
-      </v-btn>
-      <input type="file" accept="application/pdf" @change="estimateSelected" />
+      <div>
+        <input
+          style="display: none"
+          ref="file2"
+          type="file"
+          accept="application/pdf"
+          @change="estimateSelected"
+        />
+        <v-btn
+          class="mr-5"
+          variant="flat"
+          rounded="1"
+          color="primary"
+          prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
+          @click="selectEstimate"
+          >ファイル選択</v-btn
+        >
+        <v-btn
+          color="primary"
+          variant="flat"
+          rounded="1"
+          prepend-icon="mdi:mdi-arrow-up-bold-circle-outline"
+          :disabled="!isUploadedEstimate"
+          @click="readEstimate"
+        >
+          参照
+        </v-btn>
+      </div>
+      <div class="tw-text-sm py-2">{{ file2Name }}</div>
     </div>
     <div>
       <div class="tw-text-sm py-2">単価</div>
@@ -50,6 +87,10 @@ import { IAgreement } from '@/types/project';
 const props = defineProps({
   id: String,
 });
+const file1 = ref();
+const file1Name = ref('');
+const file2 = ref();
+const file2Name = ref('');
 const agreementFile = ref('');
 const estimateFile = ref('');
 const agreement = ref<IAgreement>({
@@ -84,9 +125,11 @@ const search = async () => {
 
 const fileSelected = async (event: any) => {
   agreementFile.value = event.target.files[0];
+  file1Name.value = event.target.files[0].name;
 };
 const estimateSelected = async (event: any) => {
   estimateFile.value = event.target.files[0];
+  file2Name.value = event.target.files[0].name;
 };
 
 const fileUpload = async () => {
@@ -95,10 +138,13 @@ const fileUpload = async () => {
   formData.append('file2', estimateFile.value);
   formData.append('id', String(agreement.value.id));
   formData.append('price', String(agreement.value.price));
-  formData.append('money', String(agreement.value.money));
+  formData.append('money', String(agreement.value.money.replace(/,/g, '')));
 
   agreement.value = await service.updateAgreement(Number(props.id), formData);
   agreementFile.value = '';
+  estimateFile.value = '';
+  file1Name.value = '';
+  file2Name.value = '';
 };
 
 const read = () => {
@@ -106,6 +152,13 @@ const read = () => {
 };
 const readEstimate = () => {
   window.open(import.meta.env.VITE_APP_URL + agreement.value.estimate_path, '_blank');
+};
+
+const selectAgreement = () => {
+  file1.value.click();
+};
+const selectEstimate = () => {
+  file2.value.click();
 };
 </script>
 
